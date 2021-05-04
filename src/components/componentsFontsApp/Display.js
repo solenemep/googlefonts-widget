346d8f92dc317b61c658f55e87b331b7c9d0b8ad
solenemhep <solene.pettier@gmail.com>
2021-05-04 09:44:02 +0200

    ok

--- src/components/componentsFontsApp/Display.js
+++ src/components/componentsFontsApp/Display.js
@@ -73,12 +73,16 @@ const Display = (props) => {
     <div className="col-lg-9">
       <section className="row mb-5">
         <h2 className="mb-3">
+          {choice === 'alpha' && lang === 'EN' && <span className="badge bg-danger">Alphabetical</span>}
+          {choice === 'alpha' && lang === 'FR' && <span className="badge bg-danger">Alphabétique</span>}
           {choice === 'date' && lang === 'EN' && <span className="badge bg-danger">Most recent</span>}
           {choice === 'date' && lang === 'FR' && <span className="badge bg-danger">Les plus récentes</span>}
           {choice === 'popularity' && lang === 'EN' && <span className="badge bg-danger">Most popular</span>}
           {choice === 'popularity' && lang === 'FR' && <span className="badge bg-danger">Les plus populaires</span>}
           {choice === 'trending' && lang === 'EN' && <span className="badge bg-danger">Top 10 trending</span>}
           {choice === 'trending' && lang === 'FR' && <span className="badge bg-danger">Top 10 tendances</span>}
+          {choice === 'style' && lang === 'EN' && <span className="badge bg-danger">Most styled</span>}
+          {choice === 'style' && lang === 'FR' && <span className="badge bg-danger">Les plus stylisées</span>}
         </h2>
         {loading && lang === 'EN' && <p className="text-center">loading...</p>}
         {loading && lang === 'FR' && <p className="text-center">chargement...</p>}
